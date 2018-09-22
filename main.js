0d9585951f4ed124f17f0809eeb46ad81eb6264f
Your NameAftan <aftan.merrida@gmail.com>
2018-09-22 13:39:35 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+
+const output = '';
+
+const wordOutput = () => {
+    const inputText = document.getElementById('textArea').value;
+    let inputWords = inputText.split(' ');
+    for (let i = 0; i < inputWords.length; i++) {
+        let output = inputWords[i];
+    }
+    printToDom(inputText,'firstButton');
+};
+
+const secretButton = document.getElementById('firstButton');
+secretButton.addEventListener("click", inputWords);
